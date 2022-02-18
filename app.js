126fa17681ad3843a526e4edd788417929ed81e3
Arthur Lubenets <arthur931@gmail.com>
2022-02-18 04:23:18 +0200

    level 5 - salting and hashing passwords with bcrypt.
    volnurability to hash passwords :
    1. dictionarry attacks
    2. hash-table cracks
    the same password will always turn into the same hash(hashed password), thereforce if hacker have an acsees to the db were my hashed password is saved, he can use hash-table of (password - and hash result) to reveal the passwords.
    one of the most important things to remrmber about hashing is as the number of characters in our passport increase the computation time that it takes to crack the hash increase exponentially.
    -----------
    Salting:
    Salting - is adding a random set of characters to the password and after that hashing it.
    in the db we will need to store only the "salt key" and the "hash"(hashed password).
    
    salt is not preventing dictionary attacks
    so HOW TO PREVENT dictionary attack?
    with alghoritmns as bcrypt that increase the execution time of the hashing.
    Salt Rounds - for every time increase in that number the amount of time that it takes to hash the password is doubled.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5"); // level 4 - require hashing algorithm
+const bcrypt = require("bcrypt"); // level 5 -require bcrypt
+const saltRounds = 10; // bcrypt - saltRound, as more we increase this number, it will take more time for the computer to generate those hashes.(hashed passwords)
 
 const app = express();
 
@@ -37,31 +38,35 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-	const newUser = new User({
-		email: req.body.username,
-		password: md5(req.body.password), //  level 4 - hashing password, when the user is registered - instead of saving the password we will use our hash function on the password and will save the.
-	});
-	newUser.save(function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			res.render("secrets");
-		}
+	bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+		const newUser = new User({
+			email: req.body.username,
+			password: hash,
+		});
+		newUser.save(function (err) {
+			if (err) {
+				console.log(err);
+			} else {
+				res.render("secrets");
+			}
+		});
 	});
 });
 
 app.post("/login", function (req, res) {
 	const username = req.body.username;
-	const password = md5(req.body.password); // level 4-
+	const password = req.body.password;
 
 	User.findOne({ email: username }, function (err, foundUser) {
 		if (err) {
 			console.log(err);
 		} else {
 			if (foundUser) {
-				if (foundUser.password === password) {
-					res.render("secrets");
-				}
+				bcrypt.compare(password, foundUser.password, function (err, result) {
+					if (result === true) {
+						res.render("secrets");
+					}
+				});
 			}
 		}
 	});
